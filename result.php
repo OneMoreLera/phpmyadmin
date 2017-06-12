5cb995885bf392133a9fb44724fb8ab3a4f60783
OneMoreLera <kuznetsovavaleria.s@gmail.com>
2017-06-12 13:31:43 +0300

    Create result.php

--- result.php
+++ result.php
@@ -4,7 +4,9 @@ if ($db =@mysql_connect ("pks41.ru","Student","1111"))
 {
 	mysql_select_db("kollege");
 	mysql_query("SET NAMES UTF8");
-	$res = mysql_query("select students.surname, students.name, students.patronym, groups.group from students inner join groups on students.group_id = groups.group_id where (surname like '$_GET[surname]');");
+	$res = mysql_query("select students.surname, students.name, 
+	students.patronym, groups.group from students inner join groups on 
+	students.group_id = groups.group_id where (surname like '$_GET[surname]');");
 	$coll = mysql_num_rows($res);
 	for ($i=0; $i<$coll; $i ++)
 	{
