a313ade9703745f2b96a5e4b864bd1994d3ebb98
OneMoreLera <kuznetsovavaleria.s@gmail.com>
2017-06-12 15:06:37 +0300

    comm2
    sub

--- result.php
+++ result.php
@@ -1,16 +1,21 @@
 <?php
 echo "<div align =\"center\">Результат поиска</div>";
-if ($db =@mysql_connect ("pks41.ru","Student","1111"))
+if ($db =@mysql_connect ("127.0.0.1","Student","1111"))
 {
-	mysql_select_db("kollege");
+	mysql_select_db("college");
 	mysql_query("SET NAMES UTF8");
-	$res = mysql_query("select students.surname, students.name, students.patronym, groups.group from students inner join groups on students.group_id = groups.group_id where (surname like '$_GET[surname]');");
+	$res = mysql_query("select students.surname, students.name, students.patronym, students.number, groups.group from students inner join groups on students.group = groups.group_id where (surname like '%$_GET[surname]%')and number like '%$_GET[number]%';");
 	$coll = mysql_num_rows($res);
+	if ($coll == 0)
+	{ echo "<div align = \"center\">Неправильно указаны данные</div><br>";
+		echo "<div align = \"center\"><a href = \"index.php\"> Назад </a></div>";
+	}
 	for ($i=0; $i<$coll; $i ++)
 	{
 		echo mysql_result($res, $i, "surname"), "&nbsp;";
 		echo mysql_result($res, $i, "name"),"&nbsp;";
 		echo mysql_result($res, $i, "patronym"),"&nbsp;";
+		echo mysql_result($res, $i, "number"),"&nbsp;";
 		echo mysql_result($res, $i, "group"),"<br>";
 	}
 	mysql_close($db);
@@ -18,5 +23,10 @@ if ($db =@mysql_connect ("pks41.ru","Student","1111"))
 else{
 	echo "<div align\"center\">Нет подключения к базе данных</div>";
 }
+function back()
+{
+Header("Location: index.php");
+        exit; 	
+};
 ?>
 	
