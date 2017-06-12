8e96dfc1a744f8c6dbfb0a958307c1f10d4f1d45
OneMoreLera <kuznetsovavaleria.s@gmail.com>
2017-06-12 15:55:02 +0300

    c3
    Des

--- result.php
+++ result.php
@@ -1,4 +1,17 @@
 <?php
+echo "<style type=\"text/css\">
+   TABLE { 
+    width: 300px; /* Ширина таблицы */
+    border-collapse: collapse; /* Убираем двойные линии между ячейками */
+   }
+   TD, TH {
+    padding: 3px; /* Поля вокруг содержимого таблицы */
+    border: 1px solid black; /* Параметры рамки */
+   }
+   TH {
+    background: #b0e0e6; /* Цвет фона */
+   }
+  </style>";
 echo "<div align =\"center\">Результат поиска</div>";
 if ($db =@mysql_connect ("127.0.0.1","Student","1111"))
 {
@@ -10,15 +23,34 @@ if ($db =@mysql_connect ("127.0.0.1","Student","1111"))
 	{ echo "<div align = \"center\">Неправильно указаны данные</div><br>";
 		echo "<div align = \"center\"><a href = \"index.php\"> Назад </a></div>";
 	}
-	for ($i=0; $i<$coll; $i ++)
-	{
-		echo mysql_result($res, $i, "surname"), "&nbsp;";
-		echo mysql_result($res, $i, "name"),"&nbsp;";
-		echo mysql_result($res, $i, "patronym"),"&nbsp;";
-		echo mysql_result($res, $i, "number"),"&nbsp;";
-		echo mysql_result($res, $i, "group"),"<br>";
+	else{
+		echo "<div align = \"center\"> <table>
+				<thead>
+				<tr>
+					<th>Фамилия</th>
+					<th>Имя</th>
+					<th>Отчество</th>
+					<th>Номер</th>
+					<th>Группа</th>
+				</tr>
+				</thead>
+				<tbody>
+		";
+		
+		for ($i=0; $i<$coll; $i ++)
+		{
+			echo"<tr>";
+			echo "<td>",mysql_result($res, $i, "surname"), "&nbsp;","</td>";
+			echo "<td>",mysql_result($res, $i, "name"),"&nbsp;","</td>";
+			echo "<td>",mysql_result($res, $i, "patronym"),"&nbsp;","</td>";
+			echo "<td>",mysql_result($res, $i, "number"),"&nbsp;","</td>";
+			echo "<td>",mysql_result($res, $i, "group"),"</td>";
+			echo "</tr>";
+		}
+		mysql_close($db);
+		echo"</tbody>
+			</table></div>";
 	}
-	mysql_close($db);
 }
 else{
 	echo "<div align\"center\">Нет подключения к базе данных</div>";
